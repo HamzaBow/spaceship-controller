8dcc70bb37ae2010b7d58dffebc5c854c2bdb50e
Hamza Bow <hamzabow77@gmail.com>
2022-06-02 18:50:01 +0100

    fix: controlling buttons layout

--- src/components/controllers/ButtonsController.tsx
+++ src/components/controllers/ButtonsController.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from "react";
 import { useDispatch } from "react-redux";
 import {
   moveForward,
@@ -8,13 +9,18 @@ import {
 
 const ButtonsController = () => {
   const dispatch = useDispatch();
+  const containerStyle: CSSProperties = {
+    display: "grid",
+    gridTemplateRows: "1fr 1fr 1fr",
+    gridTemplateColumns: "1fr 1fr",
+  };
   return (
-    <>
-      <button onClick={() => dispatch(moveForward())}>Forward</button>
-      <button onClick={() => dispatch(moveBackward())}>Backward</button>
-      <button onClick={() => dispatch(rotateLeft())}>Rotate left</button>
-      <button onClick={() => dispatch(rotateRight())}>Rotate right</button>
-    </>
+    <div style={containerStyle}>
+      <button style={{ gridArea: "1/2/2/3" }} onClick={() => dispatch(moveForward())}>Forward</button>
+      <button style={{ gridArea: "2/2/3/3" }} onClick={() => dispatch(moveBackward())}>Backward</button>
+      <button style={{ gridArea: "2/1/3/2" }} onClick={() => dispatch(rotateLeft())}>Rotate left</button>
+      <button style={{ gridArea: "2/3/3/4" }} onClick={() => dispatch(rotateRight())}>Rotate right</button>
+    </div>
   )
 }
 
