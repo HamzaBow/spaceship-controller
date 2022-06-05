95ed302d96081a9201040f297069bc629041833d
Hamza Bow <hamzabow77@gmail.com>
2022-06-05 20:29:07 +0100

    style: tiny style adjustments

--- src/components/controllers/ObstacleController.tsx
+++ src/components/controllers/ObstacleController.tsx
@@ -8,7 +8,7 @@ const ObstacleController = () => {
     dispatch(putObstacles())
   }
   return (
-      <button onClick={handleClick}>Generate Random Obstacles</button>
+      <button className='btn-gen-obs' onClick={handleClick}>Generate Random Obstacles</button>
   )
 }
 
